199bd6c2369c0ffbc2431df18606bb0e151e1e7c
YogliB <yogiboaron@gmail.com>
2021-02-13 09:28:54 +0200

    add external-scss example

---
+++ examples/external-scss/src/components/Button.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { MDCRipple } from "@material/ripple";
+  import { onDestroy, onMount } from "svelte";
+  import { compute_rest_props } from "svelte/internal";
+
+  let buttonEl;
+  let buttonRipple;
+
+  onMount(() => {
+    buttonRipple = new MDCRipple(buttonEl);
+  });
+
+  onDestroy(() => {});
+</script>
+
+<div class="mdc-touch-target-wrapper">
+  <button bind:this={buttonEl} class="mdc-button mdc-button--touch">
+    <span class="mdc-button__ripple" />
+    <span class="mdc-button__label">My Accessible Button</span>
+    <span class="mdc-button__touch" />
+  </button>
+</div>
+
+<style lang="scss">
+  :global {
+    @use "@material/button/styles";
+  }
+</style>
